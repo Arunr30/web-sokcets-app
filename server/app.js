4c2c5a11369e6ecb6422e2308a67b31017988da4
Arunvasuacchu <arunvasur@gmail.com>
2025-03-11 22:51:38 +0530

    implemented group feautures

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-const {Server} = require('socket.io') // why socket io, because websocket is very low level it annot broadcast to all users. socket io helps us fall back to long polling
+import { Server } from "socket.io"; // why socket io, because websocket is very low level it annot broadcast to all users. socket io helps us fall back to long polling
+import express from "express"
 
-const http = require("http")
+const app = express();
 
-const httpServer = http.createServer();
-httpServer.listen(3002)
+const httpServer = app.listen(3002, () => {
+  console.log("server is listening");
+  
+});
+
+
 
 // instantiated
 
@@ -15,7 +20,20 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log(`user ${socket.id} is connected`)
-  
+  // upon connection - only to the current user
+  socket.emit("message", "welcome to the chat app")
+
+  // upon conncetion to all the user
+
+  socket.broadcast.emit("message", `${socket.id.substring(0, 5)}: is connected"`)
+
+  // upon disconnection
+
+  socket.broadcast.emit("message", `${socket.id.substring(0, 5)}: is disconnected"`)
+
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name)
+  })
   socket.on("message", (data) => {
     console.log(data);
     io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
